ae062dd670fb73f981055872f8822b5d8732e18f
Suraj Jagdev <39743409+surajjagdev@users.noreply.github.com>
2026-05-03 16:19:03 -0400

    Add: binary-tree-diameter - submission-4

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-4.go
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func diameterOfBinaryTree(root *TreeNode) int {
+    res := 0
+
+	var dfs func(*TreeNode) int
+
+	dfs = func(node *TreeNode) int {
+		if node == nil {
+			return 0
+		}
+
+		left := dfs(node.Left)
+		right := dfs(node.Right)
+
+		res = max(res, left + right)
+
+		return 1 + max(left, right)
+	}
+
+	dfs(root)
+
+
+	return res
+}
